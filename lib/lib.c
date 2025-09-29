4e03823538aa9595836a173f8775bbaf41a8f3ef
Andreea-Daniela Belu <119062819+andreea-belu25@users.noreply.github.com>
2025-09-29 15:40:22 +0300

    Update lib.c

--- lib/lib.c
+++ lib/lib.c
@@ -28,6 +28,7 @@ int get_sock(const char *if_name)
 
 	struct ifreq intf;
 	strcpy(intf.ifr_name, if_name);
+	
 	res = ioctl(s, SIOCGIFINDEX, &intf);
 	DIE(res, "ioctl SIOCGIFINDEX");
 
@@ -38,6 +39,7 @@ int get_sock(const char *if_name)
 
 	res = bind(s, (struct sockaddr *)&addr , sizeof(addr));
 	DIE(res == -1, "bind");
+	
 	return s;
 }
 
@@ -50,6 +52,7 @@ int send_to_link(int intidx, char *frame_data, size_t length)
 	int ret;
 	ret = write(interfaces[intidx], frame_data, length);
 	DIE(ret == -1, "write");
+	
 	return ret;
 }
 
@@ -57,6 +60,7 @@ ssize_t receive_from_link(int intidx, char *frame_data)
 {
 	ssize_t ret;
 	ret = read(interfaces[intidx], frame_data, MAX_PACKET_LEN);
+	
 	return ret;
 }
 
@@ -68,15 +72,18 @@ int socket_receive_message(int sockfd, char *frame_data, size_t *len)
 	 * */
 	int ret = read(sockfd, frame_data, MAX_PACKET_LEN);
 	DIE(ret < 0, "read");
+	
 	*len = ret;
 	return 0;
 }
 
-int recv_from_any_link(char *frame_data, size_t *length) {
+int recv_from_any_link(char *frame_data, size_t *length)
+{
 	int res;
 	fd_set set;
 
 	FD_ZERO(&set);
+	
 	while (1) {
 		for (int i = 0; i < ROUTER_NUM_INTERFACES; i++) {
 			FD_SET(interfaces[i], &set);
@@ -90,6 +97,7 @@ int recv_from_any_link(char *frame_data, size_t *length) {
 			if (FD_ISSET(interfaces[i], &set)) {
 				ssize_t ret = receive_from_link(i, frame_data);
 				DIE(ret < 0, "receive_from_link");
+				
 				*length = ret;
 				return i;
 			}
@@ -103,13 +111,16 @@ char *get_interface_ip(int interface)
 {
 	struct ifreq ifr;
 	int ret;
+	
 	if (interface == 0)
 		sprintf(ifr.ifr_name, "rr-0-1");
 	else {
 		sprintf(ifr.ifr_name, "r-%u", interface - 1);
 	}
+	
 	ret = ioctl(interfaces[interface], SIOCGIFADDR, &ifr);
 	DIE(ret == -1, "ioctl SIOCGIFADDR");
+	
 	return inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr);
 }
 
@@ -117,13 +128,16 @@ void get_interface_mac(int interface, uint8_t *mac)
 {
 	struct ifreq ifr;
 	int ret;
+	
 	if (interface == 0)
 		sprintf(ifr.ifr_name, "rr-0-1");
 	else {
 		sprintf(ifr.ifr_name, "r-%u", interface - 1);
 	}
+	
 	ret = ioctl(interfaces[interface], SIOCGIFHWADDR, &ifr);
 	DIE(ret == -1, "ioctl SIOCGIFHWADDR");
+	
 	memcpy(mac, ifr.ifr_addr.sa_data, 6);
 }
 
@@ -131,8 +145,10 @@ static int hex2num(char c)
 {
 	if (c >= '0' && c <= '9')
 		return c - '0';
+	
 	if (c >= 'a' && c <= 'f')
 		return c - 'a' + 10;
+	
 	if (c >= 'A' && c <= 'F')
 		return c - 'A' + 10;
 
@@ -142,9 +158,11 @@ static int hex2num(char c)
 int hex2byte(const char *hex)
 {
 	int a, b;
+	
 	a = hex2num(*hex++);
 	if (a < 0)
 		return -1;
+	
 	b = hex2num(*hex++);
 	if (b < 0)
 		return -1;
@@ -155,18 +173,24 @@ int hex2byte(const char *hex)
 int hwaddr_aton(const char *txt, uint8_t *addr)
 {
 	int i;
+	
 	for (i = 0; i < 6; i++) {
 		int a, b;
+		
 		a = hex2num(*txt++);
 		if (a < 0)
 			return -1;
+		
 		b = hex2num(*txt++);
 		if (b < 0)
 			return -1;
+		
 		*addr++ = (a << 4) | b;
+		
 		if (i < 5 && *txt++ != ':')
 			return -1;
 	}
+	
 	return 0;
 }
 
@@ -183,10 +207,12 @@ uint16_t checksum(uint16_t *data, size_t length)
 {
 	unsigned long checksum = 0;
 	uint16_t extra_byte;
+	
 	while (length > 1) {
 		checksum += ntohs(*data++);
 		length -= 2;
 	}
+	
 	if (length) {
 		*(uint8_t *)&extra_byte = *(uint8_t *)data;
 		checksum += extra_byte;
@@ -194,6 +220,7 @@ uint16_t checksum(uint16_t *data, size_t length)
 
 	checksum = (checksum >> 16) + (checksum & 0xffff);
 	checksum += (checksum >>16);
+	
 	return (uint16_t)(~checksum);
 }
 
@@ -206,6 +233,7 @@ int read_rtable(const char *path, struct route_table_entry *rtable)
 	while (fgets(line, sizeof(line), fp) != NULL) {
 		p = strtok(line, " .");
 		i = 0;
+		
 		while (p != NULL) {
 			if (i < 4)
 				*(((unsigned char *)&rtable[j].prefix)  + i % 4) = (unsigned char)atoi(p);
@@ -218,6 +246,7 @@ int read_rtable(const char *path, struct route_table_entry *rtable)
 
 			if (i == 12)
 				rtable[j].interface = atoi(p);
+			
 			p = strtok(NULL, " .");
 			i++;
 		}
@@ -228,26 +257,36 @@ int read_rtable(const char *path, struct route_table_entry *rtable)
 
 		j++;
 	}
+	
 	return j;
 }
 
 int parse_arp_table(char *path, struct arp_table_entry *arp_table)
 {
 	FILE *f;
+	
 	fprintf(stderr, "Parsing ARP table\n");
+	
 	f = fopen(path, "r");
 	DIE(f == NULL, "Failed to open %s", path);
+	
 	char line[100];
 	int i = 0;
+	
 	for(i = 0; fgets(line, sizeof(line), f); i++) {
 		char ip_str[50], mac_str[50];
 		sscanf(line, "%s %s", ip_str, mac_str);
+		
 		fprintf(stderr, "IP: %s MAC: %s\n", ip_str, mac_str);
+		
 		arp_table[i].ip = inet_addr(ip_str);
+		
 		int rc = hwaddr_aton(mac_str, arp_table[i].mac);
 		DIE(rc < 0, "invalid MAC");
 	}
+	
 	fclose(f);
 	fprintf(stderr, "Done parsing ARP table.\n");
+	
 	return i;
 }
